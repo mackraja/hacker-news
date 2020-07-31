05adaa6a2e011380e649c4e94fe40a1fb11ebd84
mackraja <montykhanna007@hotmail.com>
2020-08-01 00:46:58 +0530

    :zap:

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,4 @@
 import AlertContainer from './AlertContainer';
-import SearchInput from './SearchInput';
-import StatusBullet from './StatusBullet';
-
 export {
-    AlertContainer,    
-    SearchInput,
-    StatusBullet,
+    AlertContainer,
 }
